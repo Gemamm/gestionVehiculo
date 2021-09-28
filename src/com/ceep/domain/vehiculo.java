d84e710ae345f975f713ca78dccda9465a050732
gemamm <gemamm1988@gmail.com>
2021-09-28 11:48:51 +0200

    Initial commit

---
+++ src/com/ceep/domain/vehiculo.java
@@ -0,0 +1,77 @@
+
+package com.ceep.domain;
+
+
+
+// Clase vehiculo
+
+public class vehiculo {
+    
+    String matricula, marca, modelo, color;
+    double tarifa;
+    boolean disponible;
+
+    public vehiculo() {
+    }
+
+    public vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    public void mostrar()
+    {
+        System.out.println("Matricula: "+matricula);
+        System.out.println("Marca: "+marca);
+        System.out.println("Modelo: "+modelo);
+        System.out.println("Color: "+color);
+        System.out.println("Tarifa: "+tarifa);
+        System.out.println("Disponible: "+disponible);
+        
+    }
+
+    @Override
+    public String toString() {
+        return "vehiculo:\n" + "matricula:" + matricula + "\nmarca:\t" + marca + "\nmodelo:\t" + modelo + "\ncolor:\t" + color + "\ntarifa:\t" + tarifa + "\ndisponible:\t" + disponible ;
+    }
+     
+    
+    }
+    
+
---
+++ src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,19 @@
+
+package com.ceep.test;
+import com.ceep.domain.vehiculo;
+
+
+public class PruebaGestion 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Vehiculo 1: ");
+        vehiculo v1  = new vehiculo ("2546-MNH", "Opel", "Corsa","Azul", 25.95, true);
+       v1.mostrar();
+        //System.out.println(v1);
+        
+    }
+    
+}
